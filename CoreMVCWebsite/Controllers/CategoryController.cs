6c641fd73d9e78fc01b8a313f09860c16fba6837
Bilal Ahmad <bilal.ahmad66229@gmail.com>
2023-07-25 18:20:59 +0500

    Add project files.

---
+++ CoreMVCWebsite/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using CoreMVCWebsite.Data;
+using CoreMVCWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCWebsite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context) {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> getCategories = _context.Categories.ToList();
+            return View(getCategories);
+        }
+    }
+}
---
+++ CoreMVCWebsite/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using CoreMVCWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMVCWebsite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Laptop", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "TV", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "AC", DisplayOrder = 3 }
+                );
+        }
+    }
+}
+
+
+
+// update-database
+// add-migration AddCategoryTableToDb
+// update-database
